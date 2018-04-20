97e80c110e3e3df6144f26fa3d0a2f9fa41b80c8
Jay Gheewala <jgheewala@snaproute.com>
2018-04-20 07:17:03 -0700

    updated docker config file and add tmux-conf support

--- docker_utility.sh
+++ docker_utility.sh
@@ -5,12 +5,6 @@ get_container_id() {
     container=$(docker ps | grep $1 | grep opt |  awk '{print $1}')
 }
 
-get_all_container_id() {
-    echo searching container id for $1
-    container=$(docker ps -a | grep $1 | grep opt |  awk '{print $1}')
-}
-
-
 function fs_login {
     if [[ $1 == "" ]]; then
         echo "please specify the container name, for e.g: asicd, intfd, adjd, etc..."
@@ -18,7 +12,7 @@ function fs_login {
     fi
     get_container_id $1
     echo logging into conatiner:$1 and id: $container
-    docker exec -it $container /bin/bash
+    docker exec -it $container /bin/bash -c "export COLUMNS=tput cols; export LINES=tput lines && reset -w && bash"
 }
 
 function fs_logs {
@@ -32,13 +26,27 @@ function fs_logs {
     docker logs $container
 }
 
-function fs_docker_ps_all_logs {
+function fs_dmn_logs {
     if [[ $1 == "" ]]; then
         echo "please specify the container name, for e.g: asicd, intfd, adjd, etc..."
         return
     fi
-    get_all_container_id $1
+    get_container_id $1
     echo dumping logs for conatiner:$1 and id: $container
-    echo docker logs $container 
-    docker logs $container
+    echo docker logs $container
+    docker exec -it $container /bin/bash -c "export COLUMNS=tput cols; export LINES=tput lines && reset -w && cat /var/log/flexswitch/$1.log"
+}
+
+function fs_container_list {
+    echo docker ps --format "table {{.ID}}\t{{.Names}}" | grep -v POD
+    docker ps --format "table {{.ID}}\t{{.Names}}" | grep -v POD
+}
+
+function fs_container_running {
+    echo docker ps --format "table {{.ID}}\t{{.Names}}" | grep -v POD
+    docker ps | grep -v pause
+}
+
+function fs_clean_logs {
+    echo /dev/null > /mnt/data/flexswitch/$1/logs/$1.log
 }
